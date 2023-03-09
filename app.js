8991ab8e88248ca9576e6cbc50b472276f6035c5
theopmw <theowrightdev@gmail.com>
2023-03-09 15:00:27 +0000

    Add content

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      fullName: 'Theo Wright',
+      age: 34,
+      photo:
+        'https://media.licdn.com/dms/image/C4D03AQGC2Iv28a8iiQ/profile-displayphoto-shrink_400_400/0/1644459946675?e=1683763200&v=beta&t=FNtv8p3lvNGSuTmBH8tAEgf8KNyzeXKtCINz0GAbuYE',
+    };
   },
-  methods: {},
+  methods: {
+    ageIn5Years() {
+      return this.age + 5;
+    },
+    favouriteNumber() {
+      return Math.floor(Math.random() * 10);
+    },
+  },
 });
 
-app.mount('');
+app.mount('#assignment');
